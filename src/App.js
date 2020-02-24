0b9ee54b5772750bceacc48fd5a666af06116d91
Lenni Korhonen <lenni.korhonen2@gmail.com>
2020-02-24 12:52:43 +0200

    Components update

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Todolist from './components/Todolist';
 
 function App() {
-
-  const [todo, setTodo] = useState({desc: '', date: ''});
-  const [todos, setTodos] = useState([]);
-
-  const addTodo = (event) => {
-    event.preventDefault();
-    setTodos([...todos, todo]);
-  }
-
-  const inputChanged = (event) => {
-    setTodo({...todo, [event.target.name]: event.target.value});
-  }
-
-  const deleteTodo = index => { 
-    setTodos(todos.filter((todo, i) => i !== index));
-  }
-
   return (
     <div className="App">
-      <div className="box black">Simple todo list</div>
-      <form onSubmit={addTodo}>
-      <fieldset>
-        <legend>Add todo:</legend>
-        Description: <input type="text" name="desc" onChange={inputChanged} value={todo.desc}/>
-        Date: <input type="date" name="date" onChange={inputChanged} value={todo.date}/>
-        <input type="submit" value="Add" />
-        </fieldset>
-      </form>
-      <Todolist todos={todos} deleteTodo={deleteTodo}/>
+      <Todolist />
     </div>
   );
 }
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-export default function Todolist(props) {
+export default function Todolist() {
+
+  const [todo, setTodo] = useState({desc: '', date: ''});
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+  }
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+  }
+
+  const deleteTodo = index => { 
+    setTodos(todos.filter((todo, i) => i !== index));
+  }
 
     return (
-        <div>
-            <table>
+        <div><div className="box black">Simple todo list</div>
+        <form onSubmit={addTodo}>
+        <fieldset>
+          <legend>Add todo:</legend>
+          Description: <input type="text" name="desc" onChange={inputChanged} value={todo.desc}/>
+          Date: <input type="date" name="date" onChange={inputChanged} value={todo.date}/>
+          <input type="submit" value="Add" />
+          </fieldset>
+        </form>
+      <table>
         <tbody>
         <th>Description</th>
         <th>Date</th>
           {
-          props.todos.map((todo, index) => 
+          todos.map((todo, index) => 
             <tr key={index}>
               <td>{todo.desc}</td>
               <td>{todo.date}</td>
-              <td><button onClick={() => props.deleteTodo(index)}>Delete</button></td>
+              <td><button onClick={() => deleteTodo(index)}>Delete</button></td>
             </tr>)}
         </tbody>
       </table>
-        </div>
+      </div>
     )
 }
